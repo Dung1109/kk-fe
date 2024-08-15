e11218e2af50395e3642d2324099d5344a950b60
Dung1109 <124704925+Dung1109@users.noreply.github.com>
2024-08-15 09:24:14 +0700

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="flex flex-col items-center justify-center w-screen h-screen">
+            <h2>Todo App</h2>
+        </div>
+    );
+}
